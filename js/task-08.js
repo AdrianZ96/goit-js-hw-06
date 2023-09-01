1d0e808d95bc611bf2acdee807d633db91e9a710
AdrianZ96 <adrianzdziarski96@wp.pl>
2023-09-01 11:54:57 +0200

    final
    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    const formData = new FormData(loginForm); 
+
+    const formObject = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+
+    if (!formObject.email || !formObject.password) {
+      alert('Wszystkie pola powinny być wypełnione');
+    } else {
+      console.log('Dane z formularza:', formObject);
+      loginForm.reset(); 
+    }
+  });
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,37 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
+
+  const input = document.querySelector('input');
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const boxesContainer = document.querySelector('#boxes');
+
+  createButton.addEventListener('click', () => {
+    const amount = input.value;
+    createBoxes(amount);
+  });
+
+  destroyButton.addEventListener('click', destroyBoxes);
+
+  function createBoxes(amount) {
+    const initialSize = 30;
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      const size = initialSize + i * 10;
+      box.style.width = size + 'px';
+      box.style.height = size + 'px';
+      box.style.backgroundColor = getRandomHexColor();
+      fragment.appendChild(box);
+    }
+
+    boxesContainer.appendChild(fragment);
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = '';
+  }
